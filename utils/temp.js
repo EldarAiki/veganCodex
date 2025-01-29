bfed63d7aa673ea8c4d6449369790fe0051bed4e
Eldar <loshinas@gmail.com>
2025-01-29 21:16:18 +0200

    removed temp file

--- utils/temp.js
+++
@@ -1,7 +0,0 @@
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUwN2YxZjc3YmNmODZjZDc5OTQzOTAxNCIsImlhdCI6MTczNzk5NTA2NywiZXhwIjoxNzQwNTg3MDY3fQ.3P2bUHXKMqWsvoDwfSAMg4TugzndmvAGZ-yFri4jmAg"
-const ecofirend = {
-    _id: "507f1f77bcf86cd799439014",
-    username: "EcoFoodie",
-    email: "noah@vegan.com",
-    password: "greenearth321"
-  }
